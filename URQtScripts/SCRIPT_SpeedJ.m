a81d3ee716cc1eb1bc7dc453f929f6424e3f4b58
kutzer <kutzer@usna.edu>
2022-02-17 11:32:41 -0500

    Updated ServoJ and SpeedJ test scripts

--- URQtScripts/SCRIPT_SpeedJ.m
+++ URQtScripts/SCRIPT_SpeedJ.m
@@ -31,3 +31,84 @@ for i = 1:10
     % Wait for a portion of the blocking time
     pause(0.7*ur.BlockingTime);
 end
+
+%% Wait for the last SpeedJ to end
+while ur.isMoving
+    % Wait...
+end
+
+%% Move to home configuration
+ur.Home;
+
+%% Create a more interesting path (joint space)
+% Set initial joint configuration
+q0 = ur.Joints;
+% Set final joint configuration
+qf = zeros(6,1);
+
+% Define movement time
+t0 = 0;
+delta_q = norm(qf - q0,inf);
+tf = delta_q/dq_max;
+
+% Fit continuous trajectory
+t = [t0,tf];
+q = [q0,qf];
+dq = zeros(6,2);
+ddq = zeros(6,2);
+pp = fitpp(t,q,t,dq,t,ddq);
+
+% Plot trajectory
+tt = linspace(t0,tf,1000);
+plotpp(pp,tt,2);
+
+%% Execute move
+ur.JointAcc = 2;
+ur.Joints = q0;
+
+dpp = diffpp(pp);
+ddpp = diffpp(dpp);
+
+dt = 0.05;
+t = t0:dt:tf;
+dq = ppval(dpp,t);
+ddq = ppval(ddpp,t);
+
+ur.BlockingTime = 2*dt;
+
+for i = 1:size(dq,2)
+    ur.JointAcc = norm(ddq(:,i),inf);
+    dq_now = dq(:,i);
+    % Execute ServoJ
+    ur.SpeedJ(dq_now);
+    % Wait for dt
+    pause(0.8*dt);
+end
+
+%% Define end-effector poses for each joint configuration
+H0 = UR_fkin(ur.URmodel,q0);
+Hf = UR_fkin(ur.URmodel,qf);
+
+%% Define internal function(s)
+function v = pose2task(H)
+% POSE2TASK maps a pose to task space using a decoupled log map of SO(3)
+
+R = H(1:3,1:3);
+d = H(1:3,4);
+k = vee( logSO(R) );
+
+v = [k; d];
+end
+
+function H = task2pose(v)
+% TASK2POSE maps a task space configuration to end-effector pose using a
+% decoupled exponential map of SO(3)
+
+k = v(1:3,:);
+d = v(4:6,:);
+R = expSO( wedge(k) );
+
+H = eye(4);
+H(1:3,1:3) = R;
+H(1:3,4) = d;
+end

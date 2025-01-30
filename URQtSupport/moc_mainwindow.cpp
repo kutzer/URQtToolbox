fe805cea5808eefb85467f1134762ba7e7f13376
kutzer <kutzer@usna.edu>
2025-01-30 16:01:03 -0500

    Qt Update

--- URQtSupport/moc_mainwindow.cpp
+++ URQtSupport/moc_mainwindow.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../Roswell/mainwindow.h"
+#include "../../Roswell, Kutzer v2/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- URQtSupport/moc_netdialog.cpp
+++ URQtSupport/moc_netdialog.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../Roswell/netdialog.h"
+#include "../../Roswell, Kutzer v2/netdialog.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
